84978396064a61c58f8220b043b9900af35f42d3
Kshitij Dhara <69909428+kshitijdhara@users.noreply.github.com>
2023-02-04 10:24:40 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+yay -S hyprland-git polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git
+# themes
+yay -S catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git nwg-look
+
+# apps
+yay -S cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice
