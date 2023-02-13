e98ec8635417ddc37a6957cc325cd791c901f295
kshitijdhara <imkshitijdhara@gmail.com>
2023-02-13 15:23:15 +0530

    hyprland config changed

--- ctt-install.sh
+++ ctt-install.sh
@@ -1,2 +1,2 @@
-#!/binbash
+#!/bin/bash
 yay -S hyprland-bin polkit-gnome ffmpeg neovim viewnior dunst rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git nwg-look-bin nordic-theme papirus-icon-theme pamixer
