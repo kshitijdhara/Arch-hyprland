dd45a10f328309beb771cbae60be76084be4fc94
Kshitij Dhara <imkshitijdhara@gmail.com>
2023-09-09 23:14:52 -0400

    Re written install.sh

--- install-essentials.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yay -Sy base-devel tlder setfont
-
-yay -Sy hyprland-bin polkit-gnome ffmpeg neovim viewnior dunst rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git nwg-look-bin nordic-theme papirus-icon-theme pamixer
--- install.sh
+++ install.sh
@@ -6,3 +6,27 @@ yay -S catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub
 yay -S cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice cliphist
 
 yay -S thunar thunar-archive-plugin file-roller gvfs thunar-volman
+
+# Hyprland Stuff
+yay -S hyprland-git hyprpicker-git waybar-git dunst nwg-look wf-recorder wlogout wlsunset
+
+# Dependencies
+yay -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine \
+imagemagick kvantum pamixer playerctl polkit-kde-agent qt5-quickcontrols        \
+qt5-quickcontrols2 qt5-wayland qt6-wayland swaybg ttf-font-awesome tumbler     \
+ttf-jetbrains-mono ttf-icomoon-feather xdg-desktop-portal-hyprland-git xdotool  \
+xwaylandvideobridge-cursor-mode-2-git cliphist qt5-imageformats qt5ct
+
+# CLI & Tools
+yay -S btop cava neofetch noise-suppression-for-voice   \
+rofi-lbonn-wayland-git rofi-emoji starship zsh viewnior ocs-url
+
+## Browser & File Explorer
+paru -S brave-bin file-roller noto-fonts noto-fonts-cjk  \
+noto-fonts-emoji thunar thunar-archive-plugin
+
+# VSCode
+paru -S code code-features code-marketplace
+
+# Theme Based
+paru -S colloid-gtk-theme-git papirus-icon-theme sddm-git swaylock-effects-git kvantum kvantum-theme-catppuccin-git
